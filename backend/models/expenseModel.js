f6d95105dbf72ac004c9abda7d890e4beb52ead3
chirag shukla <shuklachirag57@gmail.com>
2024-08-15 22:50:22 +0530

    expesneModel

---
+++ backend/models/expenseModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const expenseSchema = new mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'customer',
+        unique:false
+    },
+        amount:{
+                type:Number,
+                
+        },
+        decription:{
+            type:String,
+
+        },date:{
+            type:Date,
+            default:Date.now
+        }
+
+});
+
+// Create the model
+const expense = mongoose.model('Expense', expenseSchema);
+
+module.exports = expense;
