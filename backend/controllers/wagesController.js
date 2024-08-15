e1d1e49cf0093a32d4e346eba2720fc41eeca14f
chirag shukla <shuklachirag57@gmail.com>
2024-08-15 22:47:39 +0530

    wagesave added

--- backend/controllers/wagesController.js
+++ backend/controllers/wagesController.js
@@ -1,5 +1,5 @@
 const wageModel= require("../models/wagesModel")
-
+const expenseModel = require("../models/expenseModel")
 
 const saveWage=async(req,res)=>{
     
@@ -17,11 +17,26 @@ const Wagesshow=async(req,res)=>{
     let id = req.body.id;
     const wagedata = await wageModel.find({user:id})
     res.status(200).send(wagedata)
+    console.log("showing th wages")
 
 
+}
+const saveExpense=async(req,res)=>{
+   
+    const{amount , decription , date ,id} = req.body
+    const expData = await expenseModel.create({
+        user:id,
+        amount:amount,
+        decription:decription,
+        date:date
+    })
+    await expData.save()
+    res.status(200).send(expData)
+   
 }
 
 module.exports={
     saveWage,
-    Wagesshow
+    Wagesshow,
+    saveExpense
 }
